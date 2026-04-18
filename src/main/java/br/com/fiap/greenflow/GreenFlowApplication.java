2402bbc02770264a5dc2d2c86aae4cac81683282
Gustavo <gustavo.avelar99@gmail.com>
2026-04-18 12:15:21 -0300

    Backend Rodando

---
+++ src/main/java/br/com/fiap/greenflow/GreenFlowApplication.java
@@ -0,0 +1,11 @@
+package br.com.fiap.greenflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenFlowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreenFlowApplication.class, args);
+    }
+}
---
+++ src/test/java/br/com/fiap/greenflow/GreenFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.greenflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
